3ca7b829659d914526f74acef2e3f110b2744bb9
David Rivera <david.rivera@rms.com>
2016-05-19 16:19:05 -0500

    Finish basic config. Moving to DnD

--- src/Board.js
+++ src/Board.js
@@ -3,6 +3,7 @@ var PropTypes = React.PropTypes;
 
 var Knight = require('./Knight');
 var Square = require('./Square');
+var {moveKnight, canMoveKnight} = require('./Game.js');
 
 
 var Board = React.createClass({
@@ -26,12 +27,20 @@ var Board = React.createClass({
 
     return (
       <div key={i}
-           style={{ width: '12.5%', height: '12.5%' }}>
+           style={{ width: '12.5%', height: '12.5%' }}
+           onClick={this.handleSquareClick.bind(this, x, y)}>
         <Square black={black}>
           {piece}
         </Square>
       </div>
     );
+  },
+
+
+  handleSquareClick: function (toX, toY) {
+    if (canMoveKnight(toX, toY)) {
+      moveKnight(toX, toY);
+    }
   },
 
 
--- src/Game.js
+++ src/Game.js
@@ -21,3 +21,15 @@ exports.moveKnight = function (toX, toY) {
   knightPosition = [toX, toY];
   emitChange();
 }
+
+
+exports.canMoveKnight = function (toX, toY) {
+  var x = knightPosition[0];
+  var y = knightPosition[1];
+
+  var dx = toX - x;
+  var dy = toY - y;
+
+  return (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+         (Math.abs(dx) === 1 && Math.abs(dy) === 2);
+}
